ecefde8e13d5a91562284d84df94650aee6cda4d
Bayron <bayronelieser@gmail.com>
2019-09-11 23:06:21 -0600

    Revert "Merge branch 'version-3.0' of https://github.com/ingsoft2019/SistemaDePuntos-IS802 into version-3.0"
    This reverts commit 85fc5dacb27742aa717b86381dd8f1d1be5186bf, reversing
    changes made to dc27fab57a6aa923c47179924666c20ebf33b967.

--- SistemaDePuntos/src/frmArea/mdl_rangoEdad.java
+++ SistemaDePuntos/src/frmArea/mdl_rangoEdad.java
@@ -26,6 +26,9 @@ public class mdl_rangoEdad extends javax.swing.JDialog {
     private int edadInicio;
     private int edadFin;
     
+    public mdl_rangoEdad(){}
+    
+    
     
     public mdl_rangoEdad(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
@@ -162,34 +165,33 @@ public class mdl_rangoEdad extends javax.swing.JDialog {
     }//GEN-LAST:event_txtEdadFinalKeyTyped
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
-      
-        Conexion con= new Conexion();
-        Map parametros = new HashMap();
-        
-        
-        try{
-        con.getConexion();
         int edadInicial = Integer.parseInt(getTxtEdadInicial().getText());
         int edadFinal = Integer.parseInt(getTxtEdadFinal().getText());
         
-        JOptionPane.showMessageDialog(null, edadInicial +" de " +edadFinal);
+        JOptionPane.showMessageDialog(null, edadInicial +" " +edadFinal);
         
+        Conexion con= new Conexion();
+        
+        Map parametros = new HashMap();
+        
+        
+        
+        try{
             parametros.put("EdadInicial", edadInicial);
             parametros.put("EdadFinal", edadFinal);
+       
+            con.getConexion();
         }catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error" + e.getMessage(),
                     "Error de conexion", JOptionPane.ERROR_MESSAGE);
         }
         
-      //  URL archivo = this.getClass().getResource("/reportes/report_cliente_x_rango_edada.jasper");
+        URL archivo = this.getClass().getResource("/reportes/report_cliente_x_rango_edad.jasper");
         
-        try {
-            URL archivo = this.getClass().getResource("/reportes/report_cliente_x_rango_edada.jasper");
-    
+        try {       
             JasperReport jr = (JasperReport) JRLoader.loadObject(archivo);
             JasperPrint jp = JasperFillManager.fillReport(jr,null, Conexion.getConexion());
-            JasperViewer jv = new JasperViewer(jp,false);
+            JasperViewer jv = new JasperViewer(jp, false);
             JDialog dialog = new JDialog(this);
             dialog.setContentPane(jv.getContentPane());
             dialog.setSize(jv.getSize());
@@ -198,7 +200,7 @@ public class mdl_rangoEdad extends javax.swing.JDialog {
             getClass().getResource("../imgSP/icono.png")));
             dialog.setVisible(true);
         } catch (JRException ex) {
-            Logger.getLogger(mdl_SubMenuReporte.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(mdl_rangoEdad.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println(ex);
         }
         
@@ -285,5 +287,31 @@ public class mdl_rangoEdad extends javax.swing.JDialog {
         this.txtEdadInicial = txtEdadInicial;
     }
 
-    
+    /**
+     * @return the edadInicio
+     */
+    public int getEdadInicio() {
+        return edadInicio;
+    }
+
+    /**
+     * @param edadInicio the edadInicio to set
+     */
+    public void setEdadInicio(int edadInicio) {
+        this.edadInicio = edadInicio;
+    }
+
+    /**
+     * @return the edadFin
+     */
+    public int getEdadFin() {
+        return edadFin;
+    }
+
+    /**
+     * @param edadFin the edadFin to set
+     */
+    public void setEdadFin(int edadFin) {
+        this.edadFin = edadFin;
+    }
 }
