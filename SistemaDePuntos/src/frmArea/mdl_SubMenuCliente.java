fe4ab5f4ff63ae4f5a354e4eddf332941f1f3f1d
Bayron <bayronelieser@gmail.com>
2019-09-26 01:43:25 -0600

    Terminacion de la gestion de zonas en el ,moduloConfiguracion

--- SistemaDePuntos/src/frmArea/mdl_SubMenuCliente.java
+++ SistemaDePuntos/src/frmArea/mdl_SubMenuCliente.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package frmArea;
 
-/**
- *
- * @author Luis Estrada
- */
 import Clases.Validar;
 import Clases.Zona;
 import com.toedter.calendar.JDateChooser;
