85c4251b2f5a819896f4a6269a5ef1af96c2de9d
EvertG <faris_alghazi1993@hotmail.com>
2019-09-29 16:41:38 -0600

    Charts Circular
    Chart Circular Solo de Parametrizar

---
+++ SistemaDePuntos/src/Chats/SistemasVentasPuntos/ChartsCircularResponse.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chats.SistemasVentasPuntos;
+
+public class ChartsCircularResponse {
+    private String nombrecompleto;
+    private String puntaje;
+    private String fechaNacimirnto;
+
+    /**
+     * @return the nombrecompleto
+     */
+    public String getNombrecompleto() {
+        return nombrecompleto;
+    }
+
+    /**
+     * @param nombrecompleto the nombrecompleto to set
+     */
+    public void setNombrecompleto(String nombrecompleto) {
+        this.nombrecompleto = nombrecompleto;
+    }
+
+    /**
+     * @return the puntaje
+     */
+    public String getPuntaje() {
+        return puntaje;
+    }
+
+    /**
+     * @param puntaje the puntaje to set
+     */
+    public void setPuntaje(String puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    /**
+     * @return the fechaNacimirnto
+     */
+    public String getFechaNacimirnto() {
+        return fechaNacimirnto;
+    }
+
+    /**
+     * @param fechaNacimirnto the fechaNacimirnto to set
+     */
+    public void setFechaNacimirnto(String fechaNacimirnto) {
+        this.fechaNacimirnto = fechaNacimirnto;
+    }
+
+}
+
